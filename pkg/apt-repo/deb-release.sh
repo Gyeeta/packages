1c2571cf3ff616793311594653438f878a2733b8
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-11-04 21:34:16 +0530

    More debian packaging
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- pkg/apt-repo/deb-release.sh
+++ pkg/apt-repo/deb-release.sh
@@ -6,8 +6,22 @@ if [[ ! -d ./pool/main || ! -d ./dists/stable/main/binary-amd64 || ! `ls ./pool/
 fi	
 
 set -e
+set -x 
+
+export GNUPGHOME=${PWD}/../../.pgpkeys
+
+if [ ! -d $GNUPGHOME ]; then
+	echo -e "ERROR : Missing GPG Key directory $GNUPGHOME : Please ensure this dir is available first...\n\n"
+	exit 1
+fi	
+
+if [ `gpg --list-keys 2> /dev/null | grep -cw Gyeeta` -eq 0 ]; then
+	echo -e "ERROR : Could not find the Gyeeta PGP Key in GPG Key directory $GNUPGHOME : Please ensure this key is available first...\n\n"
+	exit 1
+fi	
 
 find pool/ -name \*.deb -exec apt-ftparchive packages {} \; > dists/stable/main/binary-amd64/Packages
+
 
 gzip -c dists/stable/main/binary-amd64/Packages > dists/stable/main/binary-amd64/Packages.gz
 bzip2 -z -c dists/stable/main/binary-amd64/Packages > dists/stable/main/binary-amd64/Packages.bz2
@@ -16,4 +30,8 @@ apt-ftparchive release -o APT::FTPArchive::Release::Origin=Gyeeta -o APT::FTPArc
 			-o APT::FTPArchive::Release::Suite=stable -o APT::FTPArchive::Release::Codename=stable \
 			-o APT::FTPArchive::Release::Components=main -o APT::FTPArchive::Release::Architectures=amd64 \
 			-o APT::FTPArchive::Release::Description="Gyeeta Debian Package Repository" dists/stable > dists/stable/Release
+
+cat ./dists/stable/Release | gpg --default-key Gyeeta -abs > ./dists/stable/Release.gpg
+
+cat ./dists/stable/Release | gpg --default-key Gyeeta --clearsign -abs > ./dists/stable/InRelease
 
