7740babd1dd928126de10691d3a35ffb9c61c5d5
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-11-06 01:01:20 +0530

    Added rpm repo scripts
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- pkg/rpm-repo/rpm-release.sh
+++ pkg/rpm-repo/rpm-release.sh
@@ -5,8 +5,19 @@ if [[ ! -d ./pool || ! `ls ./pool/*.rpm 2> /dev/null | head -1` =~ \.rpm$ ]]; th
 	exit 1
 fi	
 
+if [ ! -f ../../.pgpkeys/pgp-key.private ]; then
+	echo -e "\nERROR : GPG Private Key ../../.pgpkeys/pgp-key.private not found\n\n"
+	exit 1
+fi	
+
 set -e
 set -x 
+
+gpg --import ../../.pgpkeys/pgp-key.private
+
+if [ `grep -c 4D491C04929C6424 ~/.rpmmacros 2> /dev/null` -ne 1 ]; then
+	echo -e "\n%_signature gpg\n%_gpg_name 4D491C04929C6424" >> ~/.rpmmacros
+fi	
 
 rpm --addsign ./pool/*.rpm
 
@@ -18,7 +29,11 @@ fi
 
 createrepo .
 
-gpg --detach-sign --armor repodata/repomd.xml
+gpg --detach-sign --armor ./repodata/repomd.xml
+
+set +e
+
+chown -R `stat --format="%u:%g" ./` ./repodata/
 
 echo -e "Created RPM Repo successfully...\n"
 
